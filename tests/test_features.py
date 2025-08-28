b7b10e7327d0c040292f97efa6a62c88b2bbe821
ghaida-av <ghaidaasan97@gmail.com>
2025-08-28 10:23:29 +0400

    Update test_features.py

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,1 +1,12 @@
+from phish_detector.features import get_url_length, has_ip_address, get_domain
+
+def test_url_length():
+    assert get_url_length("http://example.com") == len("http://example.com")
+
+def test_has_ip_address():
+    assert has_ip_address("http://192.168.0.1/login") is True
+    assert has_ip_address("http://example.com") is False
+
+def test_get_domain():
+    assert get_domain("http://example.com") == "example.com"
 
