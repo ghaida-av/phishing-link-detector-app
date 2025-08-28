74b166f3625f45cdadd95c4e90501081cf94451c
ghaida-av <ghaidaasan97@gmail.com>
2025-08-28 10:39:41 +0400

    Update data_split.py

--- phish_detector/data_split.py
+++ phish_detector/data_split.py
@@ -1,1 +1,10 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def split_dataset(df, label_column='label', test_size=0.2, random_state=42):
+    """
+    Split dataset into train and test sets
+    """
+    train, test = train_test_split(df, test_size=test_size, stratify=df[label_column], random_state=random_state)
+    return train, test
 
